15f5451b9f34fe27c2898ef2a859d5e8f2a811b1
YuvrajKumrawat18 <yurajkumrawat@gmail.com>
2025-10-21 15:42:52 +0530

    Implemented ConsoleAlertService for low stock Alert

--- src/main/java/com/yuvraj/warehouse/ConsoleAlertService.java
+++ src/main/java/com/yuvraj/warehouse/ConsoleAlertService.java
@@ -1,5 +1,10 @@
 package com.yuvraj.warehouse;
+import com.yuvraj.warehouse.Product;
 
-public class ConsoleAlertService {
-    
+
+public class ConsoleAlertService implements StockObserver{
+    @Override
+    public  void onLowStock(Product product){
+        System.out.println("Restock Alert: Low Stock for " + product.getName() + "- Only " + product.getQuantity() + "Left!");
+    }
 }
